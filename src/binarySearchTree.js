741c7d46649a9d3cffb22777918d7e1bd3bb9c3b
nick <112791837+NCDUBS11@users.noreply.github.com>
2025-02-19 05:26:27 -0800

    Add insert and delete methods to binarySearchTree class

--- src/binarySearchTree.js
+++ src/binarySearchTree.js
@@ -6,31 +6,123 @@
 ==buildTree(array) - takes array, sorts it, removes duplicates and creates a balanced binary tree consisting of Node objects. This should return the level-0 root node.
 --complete sorting through mergeSort(array) helper function
 
-==randomArray(length) - generates an array of specified length filled w/ random numbers
 */
 
 class Node {
-  constructor(data = null, left = null, right = null) {
-    this.data = data;
-    this.left = left;
-    this.right = right;
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
   }
 }
 
-class Tree {
-  constructor(array) {
-    this.array = array;
-    this.root = null;
+export class Tree {
+  constructor(array = null) {
+    this.root = this.buildTree(mergeSort(array));
   }
 
   buildTree(array) {
-    //takes array an turns it into a balanced binary tree filled w/ Nodes.
-    //requires sort and duplicateRemoval helper functions
+    if (array === null) return null;
+    /*
+    1) init start = 0, end = arr.length -1, mid = (start+end)/2
+    2) Create tree node with mid as root (called A in this example)
+    3)Recursively do steps 4 & 5
+    4)Calc mid of left subarray and make it root of left subtree of A
+    5)Calc mid of right subarray and make it root of right subtree of A
+    */
+    let start = 0;
+    let end = array.length - 1;
+    let middle = Math.ceil(end / 2);
+
+    if (end < 0) return null;
+
+    const treeNode = new Node(array[middle]);
+
+    treeNode.left = this.buildTree(array.slice(start, middle));
+    treeNode.right = this.buildTree(array.slice(middle + 1));
+
+    return treeNode;
+  }
+
+  insertNode(value, root = this.root) {
+    try {
+      if (typeof value !== "number") {
+        throw new Error("Node value must be a number");
+      }
+
+      if (!this.root) {
+        this.root = new Node(value);
+        return;
+      }
+
+      if (!root) {
+        return new Node(value);
+      }
+
+      if (value < root.value) {
+        root.left = this.insertNode(value, root.left);
+        return root;
+      } else if (value > root.value) {
+        root.right = this.insertNode(value, root.right);
+        return root;
+      }
+      throw new Error(`Value [${value}] already exists or is invalid.`);
+    } catch (e) {
+      console.log(e);
+      return root;
+    }
+  }
+
+  deleteNode(value, root = this.root) {
+    //Need to find value
+    //no root, value < root, value > root OR value found
+    //once found next actions depend on number of children
+    //none-remove node, 1 child-connect child to grandparent,
+    //2 children-findMin of next largest branch and swap with target
+
+    if (!root) {
+      root = null;
+    }
+
+    if (value < root.value) {
+      root.left = this.deleteNode(value, root.left);
+    } else if (value > root.value) {
+      root.right = this.deleteNode(value, root.right);
+    } else {
+      if (!root.left && !root.right) {
+        root = null;
+      } else if (!root.left) {
+        root = root.right;
+      } else if (!root.right) {
+        root = root.left;
+      } else {
+        let tempRoot = this.findMin(root.right);
+        root.value = tempRoot.value;
+        root.right = this.deleteNode(tempRoot.value, root.right);
+      }
+    }
+    return root;
+  }
+
+  findMin(root = this.root) {
+    try {
+      if (!root) {
+        throw new Error("Invalid argument");
+      }
+
+      while (root.left) {
+        root = root.left;
+      }
+      return root;
+    } catch (e) {
+      console.log(e);
+      return 0;
+    }
   }
 }
 
 //Generate random array for input to functions for testing
-function randomArray(length, range = 100) {
+export function randomArray(length = 10, range = 20) {
   if (typeof length == "number" && typeof range == "number") {
     let newArray = [];
     length = Math.floor(length);
@@ -45,6 +137,8 @@ function randomArray(length, range = 100) {
 //merge sort function, original array length may not equal output array length
 //due to duplicates being sorted out in helper function merge.
 function mergeSort(array) {
+  if (array === null) return null;
+
   if (array.length <= 1) return array;
 
   const start = array.indexOf(array[0]);
@@ -89,3 +183,26 @@ function merge(left, right) {
   }
   return mergedArray;
 }
+
+//===================================================================================
+
+// let array = randomArray(10, 200);
+
+// console.log(mergeSort(array));
+
+// const newTree = new Tree(array);
+
+export const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+// prettyPrint(newTree.root);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
 import "./styles.css";
+import { Tree, prettyPrint } from "./binarySearchTree";
 //import { function name } from "./jsFile";
 //import odinImage from "./odin.png";
 
 if (process.env.NODE_ENV !== "production") {
-    console.log("Looks like we are in development mode!");
-};
+  console.log("Looks like we are in development mode!");
+}
+
+const pracTree = new Tree();
+
+pracTree.insertNode(40);
+pracTree.insertNode(20);
+pracTree.insertNode(40);
+pracTree.insertNode(60);
+pracTree.insertNode(70);
+pracTree.insertNode(60);
+pracTree.insertNode(25);
+pracTree.insertNode(10);
+pracTree.insertNode(48);
+pracTree.insertNode(1);
+pracTree.insertNode(85);
+pracTree.insertNode(13);
+
+prettyPrint(pracTree.root);
+
+pracTree.deleteNode(60);
+pracTree.deleteNode(48);
+pracTree.deleteNode(20);
+
+prettyPrint(pracTree.root);
